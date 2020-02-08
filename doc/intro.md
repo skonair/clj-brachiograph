c1c501a2aa633aa3b9da9471f5c24addef4bbd39
Sascha Koch <sascha.koch@web.de>
2020-02-08 21:17:53 +0100

    initial commmit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-brachiograph
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_brachiograph/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-brachiograph.core-test
+  (:require [clojure.test :refer :all]
+            [clj-brachiograph.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
